30f75355616bdd03e60416d1a3acc4ae935058ac
Ростиslave <opamkgue@ukr.net>
2023-09-01 03:12:25 +0300

    review

--- src/app/modules/breeds/breeds.component.ts
+++ src/app/modules/breeds/breeds.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-breeds',
   templateUrl: './breeds.component.html',
-  styleUrls: ['./breeds.component.css']
+  styleUrls: ['./breeds.component.scss']
 })
 export class BreedsComponent {
-
+  breed: string = 'test breed';
 }
