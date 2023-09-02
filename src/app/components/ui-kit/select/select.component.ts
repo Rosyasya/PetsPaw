f8ef5aa534aca05ee75551e27f5b020f63039898
Ростиslave <opamkgue@ukr.net>
2023-09-02 04:10:46 +0300

    review

--- src/app/components/ui-kit/select/select.component.ts
+++ src/app/components/ui-kit/select/select.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-select',
@@ -6,12 +6,16 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./select.component.scss']
 })
 export class SelectComponent {
-  @Input() options: Array<any> = [];
+  @Input() options: Array<any> = [{name: 'asdasd'}, {name: 'test2'}];
   @Input() label?: string;
   @Input() placeholder: string = 'test';
+  @Input() value: string = 'test';
+  @Output() valueChange = new EventEmitter();
   open: boolean = false;
 
   selectOption(option: any) {
-
+    this.value = option.name;
+    this.valueChange.emit(option.id);
+    this.open = !this.open;
   }
 }
